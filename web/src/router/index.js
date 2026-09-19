290930e8dbd3e7c1679a627a3564396700968cbd
Rogelio Lozano <rogelio.aeloz@gmail.com>
2026-09-19 17:00:18 -0600

    feat: add GitHub OAuth integration with connection handling and repository listing

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -3,6 +3,7 @@ import Dashboard from "../views/Dashboard.vue";
 import About from "../views/About.vue";
 import Waifu from "../views/Waifu.vue";
 import Animations from "../views/Animations.vue";
+import GitHub from "../views/GitHub.vue";
 import NotFound from "../views/NotFound.vue";
 
 export default createRouter({
@@ -12,6 +13,8 @@ export default createRouter({
     { path: "/about", component: About },
     { path: "/waifu", component: Waifu },
     { path: "/animations", component: Animations },
+    // Also where the OAuth callback lands the browser once it is done.
+    { path: "/github", component: GitHub },
     { path: "/:pathMatch(.*)*", component: NotFound },
   ],
 });
